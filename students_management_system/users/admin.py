3a7f8c63b6746b6e817adb84183f94cf1d04f4ba
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-07 23:19:48 +0100

    Remove is_admin field from User model

--- students_management_system/users/admin.py
+++ students_management_system/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 class UserAdmin(BaseUserAdmin):

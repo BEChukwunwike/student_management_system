cdcdabcfa81393e9013bb14a3b06c8595f14285c
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-08 22:33:44 +0100

    refactor: Add clean method to User model for validation and remove unused code

--- students_management_system/users/models.py
+++ students_management_system/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class User(AbstractUser):
@@ -7,5 +8,11 @@ class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
     is_parent = models.BooleanField(default=False)
     
-    def __str__(self):
-        return self.username
+    def clean(self):
+        super().clean()
+        if sum([self.is_student, self.is_admin, self.is_lecturer]) > 1:
+            raise ValidationError("A user can only be a student, admin, or lecturer, not more than one.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
